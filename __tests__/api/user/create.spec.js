20572e659c88bc1b156b50e001ed6755ddb10b0f
Mihail Bodrov <connormiha1@gmail.com>
2017-04-07 00:58:52 +0300

    :white_check_mark: add tet for api user

---
+++ __tests__/api/user/create.spec.js
@@ -0,0 +1,22 @@
+// @flow
+
+import users from 'users';
+import apiCreateGame from 'api/user/create';
+import {anyObject} from 'mock/objects';
+
+describe('API user/create', () => {
+    afterEach(() => {
+        users.clear();
+    });
+
+    it('should create user', () => {
+        const json = jest.fn();
+        const response: express$Response = Object.assign(anyObject(), {json});
+        const request: express$Request = anyObject();
+
+        apiCreateGame(request, response);
+
+        expect(json).toHaveBeenCalledTimes(1);
+        expect(users.count).toBe(1);
+    });
+});
---
+++ __tests__/api/user/delete.spec.js
@@ -0,0 +1,46 @@
+// @flow
+
+import users from 'users';
+import User from 'user';
+import apiDeleteUser from 'api/user/delete';
+import {anyObject} from 'mock/objects';
+
+describe('API user/delete', () => {
+    afterEach(() => {
+        users.clear();
+    });
+
+    it('should raise error', () => {
+        const end = jest.fn();
+        const status = jest.fn();
+        const response: express$Response = Object.assign(anyObject(), {end, status});
+        const request: express$Request = Object.assign(anyObject(), {body: {id: 1}});
+
+        apiDeleteUser(request, response);
+
+        expect(end).toHaveBeenCalledTimes(1);
+        expect(status).toHaveBeenCalledWith(400);
+        expect(status).toHaveBeenCalledTimes(1);
+        expect(users.count).toBe(0);
+    });
+
+    it('should success delete exist user', () => {
+        const user = new User({id: 1});
+
+        users.add(user);
+
+        const end = jest.fn();
+        const status = jest.fn();
+        const response: express$Response = Object.assign(anyObject(), {end, status});
+        const request: express$Request = Object.assign(anyObject(), {body: {id: user.id}});
+
+        expect(users.count).toBe(1);
+
+        apiDeleteUser(request, response);
+
+        expect(end).toHaveBeenCalledTimes(1);
+        expect(status).toHaveBeenCalledWith(204);
+        expect(status).toHaveBeenCalledTimes(1);
+        expect(users.count).toBe(0);
+    });
+});
